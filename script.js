677048430e12c0ca12aa38cf6337b6d0c98a7434
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-06-16 18:13:39 +0200

    Added prompts to script

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = prompt ("Enter task 1");
+console.log(title);
+
+const description = prompt ("Enter description");
+console.log(description);
+
+const status = prompt ("Enter status");
+console.log(status);
+
+
+const title2 = prompt ("Enter task 2");
+console.log(title2);
+

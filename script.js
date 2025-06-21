268821d4fb10492f722a2eb0d9876a9c939d7e3d
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-06-21 18:18:46 +0200

    Added comments to Javascript

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let task1 = {
-  title: prompt("Enter a title for Task 1:"),
+
+// ===  Main Layout === // 
+
+
+let task1 = {  // Declared my Variable using let // 
+  title: prompt("Enter a title for Task 1:"),  // Created an array to define multiple prompts // 
   description: prompt("Enter a description for Task 1:"),
-  status: prompt("Enter a status for Task 1 TODO, DOING or DONE:").toLowerCase()
+  status: prompt("Enter a status for Task 1 TODO, DOING or DONE:").toLowerCase() // All letter cases will be automatically changed to lower case // 
+                                                                                    
 };
-if (task1.status !== "todo" && task1.status !== "doing" && task1.status !== "done"){
-  alert("Invalid Status. Please enter 'todo' 'doing' or 'done'");
+if (task1.status !== "todo" && task1.status !== "doing" && task1.status !== "done"){ // This is an If statement and logical Operators // 
+  alert("Invalid Status. Please enter 'todo' 'doing' or 'done'"); // This an alert to notify that the incorrect status has been inserted // 
 } 
 
-console.log(task1);
+console.log(task1); // This is the console log // 
 
 let task2 = {
   title: prompt("Enter a title for Task 2:"),
@@ -19,3 +24,5 @@ if (task2.status !== "todo" && task2.status !== "doing" && task2.status !== "don
 }
 
 console.log(task2);
+
+// ==== End of Main Layout ==== // 
